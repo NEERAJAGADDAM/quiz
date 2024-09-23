0dd2c140b0bbbd1bb851588f85aae5a39872108a
NEERAJAGADDAM <125481590+NEERAJAGADDAM@users.noreply.github.com>
2024-09-23 17:10:55 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,7 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    // Add your form submission logic here (e.g., AJAX request or form validation)
+    alert('Form submitted successfully!');
+});
+
+  
